c5cf42a447dddc613df2fa7f8ce8d84f338d6c40
rirodevdom <rinlumin1996@gmail.com>
2026-07-30 19:29:32 +0300

    Test materialized Hikvision media readiness patch

---
+++ test/media/readinessPatch.test.ts
@@ -0,0 +1,14 @@
+import assert from 'node:assert/strict';
+import fs from 'node:fs/promises';
+import test from 'node:test';
+
+test('media readiness patch is materialized before tests', async () => {
+  const [routes, index] = await Promise.all([
+    fs.readFile(new URL('../../src/http/mediaRoutes.ts', import.meta.url), 'utf8'),
+    fs.readFile(new URL('../../src/index.ts', import.meta.url), 'utf8')
+  ]);
+  assert.match(routes, /CHANNEL_READY_TIMEOUT_MS = 20_000/);
+  assert.match(routes, /waitForLivePlaylist/);
+  assert.match(index, /live_expected/);
+  assert.match(index, /live_ready/);
+});
